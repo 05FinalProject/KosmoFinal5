0836371e27e2597cb2ca20092c28ab22f15691fe
zzeennee <96711823+zzeennee@users.noreply.github.com>
2022-05-19 12:25:02 +0900

    일상공유페이지 서비스 수정중

--- src/main/java/com/example/dao/CommunityRepository.java
+++ src/main/java/com/example/dao/CommunityRepository.java
@@ -1,5 +1,7 @@
 package com.example.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,7 +10,7 @@ import com.example.domain.CommunityVO;
 @Repository
 public interface CommunityRepository extends CrudRepository<CommunityVO, Integer> {
 	
-	 
+	/* public List<CommunityVO> findByc_num() */
 	 
 	
 }
---
+++ src/main/java/com/example/service/community/CommunityServiceImpl.java
@@ -0,0 +1,19 @@
+package com.example.service.community;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.dao.CommunityRepository;
+import com.example.domain.CommunityVO;
+
+@Service
+public class CommunityServiceImpl implements CommunityService {
+	
+	/*
+	 * @Autowired private CommunityRepository c_repo;
+	 */
+	
+	//일상 공유 게시글 등록
+	
+
+}
