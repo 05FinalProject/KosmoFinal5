4b34635cb675147c87405b201407a8c37f0db0a4
JungYiChim <ffddfdsfa88@gmail.com>
2022-05-20 17:03:02 +0900

    테이블수정 마지막
    테이블수정 마지막

--- src/main/java/com/example/domain/BadgeVO.java
+++ src/main/java/com/example/domain/BadgeVO.java
@@ -1,12 +1,10 @@
 package com.example.domain;
 
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 
 import lombok.Data;
 
@@ -15,7 +13,7 @@ public class BadgeVO {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int b_id;
+	private Integer b_id;
 	private String b_log;
 	private String b_first;
 	private String b_fwalk;
