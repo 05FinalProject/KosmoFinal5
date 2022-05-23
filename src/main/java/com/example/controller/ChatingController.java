3b0029232897c5b0a624491755560e7bf7df36ae
JungYiChim <ffddfdsfa88@gmail.com>
2022-05-23 18:19:14 +0900

    채팅방 화면 조금 수정(다른 사람들어왔을 때)
    채팅방 화면 조금 수정(다른 사람들어왔을 때)

--- src/main/java/com/example/controller/ChatingController.java
+++ src/main/java/com/example/controller/ChatingController.java
@@ -1,7 +1,6 @@
-package com.example.controller.ChattingController;
+package com.example.controller;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -10,10 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.example.dao.ChatingRoomRepository;
 import com.example.domain.ChatingRoomVO;
 import com.example.domain.Room;
 import com.example.domain.UserVO;
@@ -98,6 +95,7 @@ public class ChatingController {
 		mv.addObject("roomName", vo.getRoomName());
 		mv.addObject("roomNumber", roomNum);
 		mv.addObject("getRoomNum",service.findByRoomName(vo).size());
+		mv.addObject("niName", service.getUserInfo(vo.getRoomMember()).getUser_nickname());
 		
 		mv.setViewName("/chating/chat");
 		
