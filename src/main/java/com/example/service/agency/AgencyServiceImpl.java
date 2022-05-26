ea519babf28c00a833007504eb1fdc44813e84b1
yoowootaek <fbdnxor1@naver.com>
2022-05-26 21:24:52 +0900

    agency 보호소 상세보기 및 호텔페이지 및 상세보기
    agency 보호소 상세보기 및 호텔페이지 및 상세보기

--- src/main/java/com/example/service/agency/AgencyServiceImpl.java
+++ src/main/java/com/example/service/agency/AgencyServiceImpl.java
@@ -8,21 +8,53 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.example.dao.AbandonedRepository;
+import com.example.dao.AgencyRepository;
 import com.example.domain.AbandonedVO;
+import com.example.domain.AgencyVO;
 
 @Service
 public class AgencyServiceImpl implements AgencyService{
 	
 	@Autowired
-	private AbandonedRepository ab;
+	private AbandonedRepository abandonedRepo;
+	
+	@Autowired
+	private AgencyRepository agencyRepo;
+
+	
 	
 	public List<AbandonedVO> getPaging(Pageable paging){
-		return ab.findAll(paging);
+		return abandonedRepo.findAll(paging);
 	}
 	
 	public int countRecord() {
-		return ab.countRecord();
+		return abandonedRepo.countRecord();
 	}
 	
+	
+	@Override
+	public List<AgencyVO> getHotelPaging(Pageable paging) {		
+		return agencyRepo.findByAgencyCategoryNum(paging, 1);
+	}
+	
+	@Override
+	public int countHotelRecord() {		
+		return agencyRepo.countHotelRecord();
+	}
+
+	//보호소 상세정보 띄우기 
+	@Override
+	public AbandonedVO getagencyShelter(AbandonedVO vo) {
+		AbandonedVO avo = abandonedRepo.findById(vo.getAbNo()).get();
+		return abandonedRepo.save(avo);
+	}
+	
+	
+	//호텔정보 상세정보 띄우기 
+		@Override
+		public AgencyVO getagencyHotel(AgencyVO vo) {
+			AgencyVO avo = agencyRepo.findById(vo.getAgencyNum()).get();
+			return agencyRepo.save(avo);
+		}
 	
 }
