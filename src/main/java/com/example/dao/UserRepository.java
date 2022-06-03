7ee7df353efd5a68a077631c6621c92d85a7ac26
limsatgat <96382179+limsatgat@users.noreply.github.com>
2022-06-03 19:27:48 +0900

    비밀번호 찾기/변경 작업 중
    비밀번호 찾기/변경 작업 중

--- src/main/java/com/example/dao/UserRepository.java
+++ src/main/java/com/example/dao/UserRepository.java
@@ -25,7 +25,17 @@ public interface UserRepository extends CrudRepository<UserVO, String>{
 	@Query(value="select user_email,user_nickname from user where user_email !=:email  order by rand() limit 18" ,nativeQuery = true)
 	List<Object[]> getRandomUsers(String email);
 	
-	
+	//select * from user where user_email='aa' and user_pass='abcd';
+	@Query("SELECT uvo FROM UserVO uvo WHERE uvo.userEmail=:userEmail and uvo.userPass=:userPass")
+	UserVO checkPass(String userEmail,String userPass);
+
+	// select * from user where user_email='aa' and user_pass='abcd' and user_phone='0102033423'
+	@Query("SELECT uvo FROM UserVO uvo WHERE uvo.userEmail=:userEmail")
+	UserVO pwSearch(String userEmail);
+
+	// UPDATE user set userPass WEHRE userEmail
+	@Query("UPDATE UserVO uvo SET uvo.userPass=:userPass WHERE uvo.userEmail=:userEmail")
+	UserVO save(String userEmail, String userPass);
 	
 	
 	
